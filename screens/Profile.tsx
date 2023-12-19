ca95e27180de3c2ce1278b07d67f20b70c279bb5
justSchwab <justin.schwab@t-online.de>
2023-12-19 19:58:02 +0100

    fetch data from mockserver on profile page

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,6 +1,8 @@
+import React, { useState, useEffect } from "react";
 import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { COLORS, SIZES, SHADOWS } from "../constants/theme";
 import { useNavigation } from "@react-navigation/native";
+import axios, { AxiosError } from "axios";
 
 const posts = [
   { name: 'kevin', source: require('../assets/images/Kevin.jpeg') },
@@ -19,11 +21,72 @@ const Post: React.FC<PostProps> = ({ image }) => (
 
 const Profile = () => {
 
-
   const navigation = useNavigation();
+  const [userNotFound, setUserNotFound] = useState(false);
+  const [notAuthorized, setNotAuthorized] = useState(false);
+  const [username, setUsername] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [status, setStatus] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+  const [followerCount, setFollowerCount] = useState("");
+  const [followingCount, setFollowingCount] = useState("");
+  const [postsCount, setPostsCount] = useState("");
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/users/:username",
+        );
+        setUsername(response.data.username);
+        setNickname(response.data.nickname);
+        setStatus(response.data.status);
+        setProfilePicture(posts[2].source);
+        setFollowerCount(response.data.follower)
+        setFollowingCount(response.data.following);
+        setPostsCount(response.data.posts);
+        
+      } catch (error) {
+        if (axios.isAxiosError(error)){
+          const axiosError = error as AxiosError;
+          if (axiosError.response){
+            switch (axiosError.response.status) {
+              case 401:
+                setNotAuthorized(true);
+                break;
+              case 404:
+                setUserNotFound(true);
+                break;
+              default:
+                console.log("Unbekannter Fehler");
+            }
+          }
+          else {
+            console.log("Netzwerkfehler oder keine Antwort vom Server");
+          }
+        }
+        else {
+          console.log("Ein unerwarteter Fehler ist aufgetreten");
+        }
+      }
+    };
 
-    return (
+    fetchUserData();
+  }, []);
+
+  if (notAuthorized) {
+    return <View style={styles.errorContainer}>
+      <Text style={styles.errorText}>Du musst dich erst anmelden</Text>
+      <TouchableOpacity style={styles.errorButton} onPress={() => navigation.navigate("Authentification" as never)}>Anmelden</TouchableOpacity>
+    </View>;
+  }
+  else if (userNotFound) {
+    return <View style={styles.errorContainer}>
+      <Text style={styles.errorText}>User not found</Text>
+    </View>;
+  }
+
+  else return (
       <ScrollView style={styles.container}>
   
   <Image
@@ -33,8 +96,9 @@ const Profile = () => {
 
         <View style={styles.header}>
           <View style={styles.userInfo}>
-            <Text style={styles.name}>DübelDoc</Text>
-            <Text style={{fontSize: SIZES.medium, color: COLORS.darkgray, marginBottom: '3%'}}>@nico01</Text>
+            <Text style={styles.name}>{nickname}</Text>
+            <Text style={{fontSize: SIZES.medium, color: COLORS.darkgray, marginBottom: '3%'}}>@{username}</Text>
+            <Text style={{marginBottom: 12, marginTop: 6}}>{status}</Text>
             <TouchableOpacity
               style={styles.editButton}
               onPress={()=> console.log("Test")}
@@ -44,15 +108,15 @@ const Profile = () => {
           </View>
           <View style={styles.stats}>
           <View style={styles.statItem}>
-              <Text style={styles.statValue}>{posts.length}</Text>
+              <Text style={styles.statValue}>{postsCount}</Text>
               <Text>Beiträge</Text>
             </View>
             <TouchableOpacity style={styles.statItem} onPress={() => navigation.navigate("FollowerList" as never)}>
-              <Text style={styles.statValue}>8</Text>
+              <Text style={styles.statValue}>{followerCount}</Text>
               <Text>Follower</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.statItem} onPress={() => navigation.navigate("FollowedList" as never)}>
-              <Text style={styles.statValue}>7</Text>
+              <Text style={styles.statValue}>{followingCount}</Text>
               <Text>Gefolgt</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.statItem} onPress={() => navigation.navigate("FriendRequest" as never)}>
@@ -75,6 +139,11 @@ const Profile = () => {
 export default Profile;
 
 const styles = StyleSheet.create({
+  errorContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: "center", alignItems: "center",
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
@@ -86,7 +155,7 @@ const styles = StyleSheet.create({
   },
   avatar: {
     width: '100%',
-    height: '33%' 
+    height: '30%' 
   },
   
   
@@ -96,7 +165,7 @@ const styles = StyleSheet.create({
   },
   name: {
     fontWeight: 'bold',
-    fontSize: 32,
+    fontSize: 28,
     marginBottom: '1%'
   },
   editButton: {
@@ -104,6 +173,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingVertical: 8,
     borderRadius: 18,
+    ...SHADOWS.medium
+  },
+  errorText: {
+    fontSize: SIZES.large,
+  },
+  errorButton: {
+    marginVertical: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 18,
+    backgroundColor: COLORS.primary,
     ...SHADOWS.medium
   },
   stats: {
