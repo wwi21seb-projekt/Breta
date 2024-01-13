913c6002077486c0968c01dde79b73c1a4479c02
Nico1808 <niconiebisch@gmail.com>
2024-01-13 18:23:25 +0100

    integrate tailwindcss (nativewind)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './screens/**/*.ts',
+    './screens/**/*.tsx',
+    './components/**/*.ts',
+    './components/**/*.tsx',
+    './navigation/**/*.ts',
+    './navigation/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
