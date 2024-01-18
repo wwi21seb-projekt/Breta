1e9c156e324ebf2b2f394f26590b8f6a1724e4a2
Nico1808 <niconiebisch@gmail.com>
2024-01-18 21:24:00 +0100

    search bar and list of results

--- env.ts
+++ env.ts
@@ -2,4 +2,4 @@
 // Aplha Server URL: https://alpha.c930.net/
 // Beta Server URL: https://www.server-beta.de/api/
 
-export const baseUrl = "http://localhost:3000/api/v1/";
+export const baseUrl = "http://192.168.0.235:3000/api/";
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,11 +1,91 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {useEffect, useState} from 'react'
+import {ScrollView, TextInput, View, TouchableOpacity, Text, SafeAreaView} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import {FlatList, Icon} from 'native-base'
+import { useDebounce } from 'use-debounce'
+import { baseUrl } from "../env";
 
-export default function Search() {
+
+
+
+const Search = () => {
+
+  // type User = {
+  //   username: string,
+  //   nickname: string,
+  //   profilePictureUrl: string
+  // }
+
+  let data: any
+
+  const [searchInput, setSearchInput] = useState('');
+  const [debouncedSearchInput] = useDebounce(searchInput, 1000);
+
+  const [showResultList, setshowResultList] = useState(false)
+  
+
+
+  useEffect(() => {
+    
+
+    (async () => {
+      const encodedSearchValue = encodeURIComponent(debouncedSearchInput)
+
+      const urlWithParams = `${baseUrl}users?username=${encodedSearchValue}&offset=0&limit=3000`
+
+      console.log(urlWithParams)
+      let response
+
+      try{
+        response = await fetch(urlWithParams, {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json'
+        }})
+        data = await response.json()
+      }catch(error){
+        console.log(error)
+      }
+
+      if (response?.ok){
+        console.log(data)
+        setshowResultList(true)
+      }
+    })();
+    console.log(debouncedSearchInput)
+  }, [debouncedSearchInput])
+
+  const handleSearchInputChange = (searchInput: string) => {
+    setSearchInput(searchInput);
+  }
+
   return (
-    <View className="flex h-screen justify-center items-center">
-      <Text className="text-xl">Suche</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className='bg-white'>
+      <View className='flex flex-row  bg-lightgray rounded-full m-4'>
+        <TextInput value={searchInput} placeholder='search for users' className='flex-1 p-4' onChangeText={handleSearchInputChange}/>
+          <TouchableOpacity className='flex items-center justify-center m-4'>
+            <Icon as={Ionicons} name="search-outline" size="xl" color="black"/>
+          </TouchableOpacity>
+      </View>
+
+      {showResultList === true && (
+        <View>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.username}
+            renderItem={({item}) => (
+              <View>
+                <Text>ist die view am arsch?</Text>
+                <Text>item.username</Text>
+                <Text>item.nickname</Text>
+              </View>
+            )}
+            >
+
+          </FlatList>
+          </View>
+      )}
+    </SafeAreaView>
   );
 }
+export default Search
