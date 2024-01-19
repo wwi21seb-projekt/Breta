ef54ea1f9c38250d00f626fc1787b4f2028fd015
Nico1808 <Nico1808@users.noreply.github.com>
2024-01-19 18:00:51 +0000

    Apply formatting changes

--- components/types/UserListTypes.ts
+++ components/types/UserListTypes.ts
@@ -1,14 +1,14 @@
 export interface User {
-    username: string;
-    nickname: string;
-    profilePictureUrl: string;
-  }
+  username: string;
+  nickname: string;
+  profilePictureUrl: string;
+}
 
 export interface ResponseData {
-    records: User[];
-    pagination: {
-        offset: number;
-        limit: number;
-        records: number;
-    };
+  records: User[];
+  pagination: {
+    offset: number;
+    limit: number;
+    records: number;
+  };
 }
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,60 +1,71 @@
-import { useEffect, useState } from 'react'
-import { TextInput, View, TouchableOpacity, Text } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
-import { FlatList, Icon } from 'native-base'
-import { useDebounce } from 'use-debounce'
+import { useEffect, useState } from "react";
+import { TextInput, View, TouchableOpacity, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList, Icon } from "native-base";
+import { useDebounce } from "use-debounce";
 import { baseUrl } from "../env";
-import { COLORS } from "../theme"
-import { User, ResponseData } from "../components/types/UserListTypes"
+import { COLORS } from "../theme";
+import { User, ResponseData } from "../components/types/UserListTypes";
 const Search = () => {
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   const [debouncedSearchInput] = useDebounce(searchInput, 1000);
-  
-  const [showResultList, setshowResultList] = useState(false)
-  const [users, setUsers] = useState<User[]>([])
-  
+
+  const [showResultList, setshowResultList] = useState(false);
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
     (async () => {
-      let data!: ResponseData
-      const encodedSearchValue = encodeURIComponent(debouncedSearchInput)
+      let data!: ResponseData;
+      const encodedSearchValue = encodeURIComponent(debouncedSearchInput);
 
-      const urlWithParams = `${baseUrl}users?username=${encodedSearchValue}&offset=0&limit=3000`
+      const urlWithParams = `${baseUrl}users?username=${encodedSearchValue}&offset=0&limit=3000`;
 
-      console.log(urlWithParams)
-      let response
+      console.log(urlWithParams);
+      let response;
 
-      try{
+      try {
         response = await fetch(urlWithParams, {
           method: "GET",
           headers: {
-            'Content-Type': 'application/json'
-        }})
-        data = await response.json()
-      }catch(error){
-        console.log(error)
+            "Content-Type": "application/json",
+          },
+        });
+        data = await response.json();
+      } catch (error) {
+        console.log(error);
       }
 
-      if (response?.ok){
-        setUsers(data.records)
-        console.log(users)
-        setshowResultList(true)
+      if (response?.ok) {
+        setUsers(data.records);
+        console.log(users);
+        setshowResultList(true);
       }
     })();
-    console.log(debouncedSearchInput)
-  }, [debouncedSearchInput])
+    console.log(debouncedSearchInput);
+  }, [debouncedSearchInput]);
 
   const handleSearchInputChange = (searchInput: string) => {
     setSearchInput(searchInput);
-  }
+  };
 
   return (
     <View>
-      <View className='bg-white'>
-        <View className='flex flex-row  bg-lightgray rounded-full m-4'>
-          <TextInput value={searchInput} placeholder='search for users' className='flex-1 p-4' onChangeText={handleSearchInputChange}/>
-            <TouchableOpacity className='flex items-center justify-center m-4'>
-              <Icon as={Ionicons} name="search-outline" size="xl" color={COLORS.black}/>
-            </TouchableOpacity>
+      <View className="bg-white">
+        <View className="flex flex-row  bg-lightgray rounded-full m-4">
+          <TextInput
+            value={searchInput}
+            placeholder="search for users"
+            className="flex-1 p-4"
+            onChangeText={handleSearchInputChange}
+          />
+          <TouchableOpacity className="flex items-center justify-center m-4">
+            <Icon
+              as={Ionicons}
+              name="search-outline"
+              size="xl"
+              color={COLORS.black}
+            />
+          </TouchableOpacity>
         </View>
       </View>
 
@@ -63,17 +74,16 @@ const Search = () => {
           <FlatList
             data={users}
             keyExtractor={(item) => item.username}
-            renderItem={({item}) => (
-            <View>
-              <Text>{item.username}</Text>
-              <Text>{item.nickname}</Text>
-            </View>
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item.username}</Text>
+                <Text>{item.nickname}</Text>
+              </View>
             )}
-            >
-          </FlatList>
-          </View>
+          ></FlatList>
+        </View>
       )}
     </View>
   );
-}
-export default Search
+};
+export default Search;
