2cc77ffb11e4a4391206d8f5ddb913323f47ebd4
justSchwab <justin.schwab@t-online.de>
2024-01-25 11:03:10 +0100

    delete unused dummyData

--- DummyData.ts
+++ DummyData.ts
@@ -1,16 +1,3 @@
-export const dummyData = [
-  {
-    username: "top_G_3",
-    nickname: "Aleks069",
-    profilePictureUrl: "",
-  },
-  {
-    username: "koenig_kemal",
-    nickname: "Kevin",
-    profilePictureUrl: "",
-  },
-];
-
 export const dummyUrls = [
   require("./assets/images/Adrian.jpeg"),
   require("./assets/images/Luca.jpeg"),
