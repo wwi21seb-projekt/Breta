98151992175394b92858be9320ba0d006afe0cbd
Koordis <AleksGeorg@outlook.de>
2024-01-25 21:36:25 +0100

    hotspot fix

--- env.ts
+++ env.ts
@@ -5,4 +5,4 @@
 // URL for development
 //export const baseUrl = "http://localhost:3000/api/";
 
-export const baseUrl = "http://192.168.0.161:3000/api/";
+export const baseUrl = "https://alpha.c930.net/api/";
