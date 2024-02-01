a965bc0535202913a1de2aae8fd3b8da3390f245
justSchwab <justSchwab@users.noreply.github.com>
2024-02-01 23:25:15 +0000

    Apply formatting changes

--- screens/Post.tsx
+++ screens/Post.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { TextInput, TouchableOpacity, View, Text, Keyboard, TouchableWithoutFeedback } from "react-native";
+import {
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { SHADOWS, COLORS } from "../theme";
 import { baseUrl } from "../env";
@@ -52,45 +59,45 @@ const PostScreen: React.FC<PostScreenprops> = ({ navigation }) => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-    <View className="bg-white flex-1">
-      <View className="bg-white justify-center flex-row">
-        <TextInput
-          className="flex-1 border-2 mt-10 ml-2.5 mr-2.5 border-lightgray rounded-[8px] p-2"
-          value={postText}
-          onChangeText={(post) => {
-            setPostText(post);
-          }}
-          multiline={true}
-          numberOfLines={8} // Anzahl der sichtbaren Zeilen
-          placeholder="Verfasse hier deinen Text ..."
-          maxLength={256}
-        />
+      <View className="bg-white flex-1">
+        <View className="bg-white justify-center flex-row">
+          <TextInput
+            className="flex-1 border-2 mt-10 ml-2.5 mr-2.5 border-lightgray rounded-[8px] p-2"
+            value={postText}
+            onChangeText={(post) => {
+              setPostText(post);
+            }}
+            multiline={true}
+            numberOfLines={8} // Anzahl der sichtbaren Zeilen
+            placeholder="Verfasse hier deinen Text ..."
+            maxLength={256}
+          />
+        </View>
+        <View className="mt-1.5 ml-2.5 justify-start flex-row">
+          <Text className="text-black text-xs">{postText.length} / 256</Text>
+        </View>
+        <View>
+          {postError.length !== 0 && (
+            <Text className="text-red pt-5 text-center">{postError}</Text>
+          )}
+        </View>
+        <View className="bg-white justify-center flex-row">
+          <TouchableOpacity
+            style={{
+              backgroundColor:
+                postText === "" ? COLORS.lightgray : COLORS.primary,
+              ...SHADOWS.small,
+            }}
+            className="flex-1 mt-[50px] mx-[120px] p-3 items-center rounded-[18px]"
+            disabled={postText === ""}
+            onPress={() => {
+              createPost();
+            }}
+          >
+            <Text className="text-black text-xs">Beitrag erstellen</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View className="mt-1.5 ml-2.5 justify-start flex-row">
-        <Text className="text-black text-xs">{postText.length} / 256</Text>
-      </View>
-      <View>
-        {postError.length !== 0 && (
-          <Text className="text-red pt-5 text-center">{postError}</Text>
-        )}
-      </View>
-      <View className="bg-white justify-center flex-row">
-        <TouchableOpacity
-          style={{
-            backgroundColor:
-              postText === "" ? COLORS.lightgray : COLORS.primary,
-            ...SHADOWS.small,
-          }}
-          className="flex-1 mt-[50px] mx-[120px] p-3 items-center rounded-[18px]"
-          disabled={postText === ""}
-          onPress={() => {
-            createPost();
-          }}
-        >
-          <Text className="text-black text-xs">Beitrag erstellen</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
     </TouchableWithoutFeedback>
   );
 };
