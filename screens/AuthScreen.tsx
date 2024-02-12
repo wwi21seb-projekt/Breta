ec29e8d60a6307cdb30e71d3b4531680ee23305a
justSchwab <justSchwab@users.noreply.github.com>
2024-02-12 20:57:40 +0000

    Apply formatting changes

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -16,7 +16,7 @@ const AuthScreen = () => {
       <View className="flex-row my-6 mx-10 rounded-xl">
         <TouchableOpacity
           style={{
-            backgroundColor: showRegistration ? COLORS.white : COLORS.primary
+            backgroundColor: showRegistration ? COLORS.white : COLORS.primary,
           }}
           className="flex-1 items-center px-3 py-1 rounded-l-xl border border-primary"
           onPress={() => showRegistration && handleButtonPress()}
@@ -25,7 +25,7 @@ const AuthScreen = () => {
         </TouchableOpacity>
         <TouchableOpacity
           style={{
-            backgroundColor: showRegistration ? COLORS.primary : COLORS.white
+            backgroundColor: showRegistration ? COLORS.primary : COLORS.white,
           }}
           onPress={() => !showRegistration && handleButtonPress()}
           className="flex-1 items-center px-3 py-1 rounded-r-xl border border-primary"
@@ -34,11 +34,7 @@ const AuthScreen = () => {
         </TouchableOpacity>
       </View>
 
-      {showRegistration ? (
-        <Register/>
-      ) : (
-        <Login />
-      )}
+      {showRegistration ? <Register /> : <Login />}
     </SafeAreaView>
   );
 };
