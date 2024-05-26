d13d3902095598f3cde6e33f9f903f8c190ab09a
AleksGeorg <AleksGeorg@users.noreply.github.com>
2024-05-26 14:29:29 +0000

    Apply formatting changes

--- components/TextPostCard.tsx
+++ components/TextPostCard.tsx
@@ -62,7 +62,6 @@ const TextPostCard: React.FC<Props> = (props) => {
   const [commentError, setCommentError] = useState<string | null>(null);
   const isAuthenticated = useCheckAuthentication();
   const [isLoginPopupVisible, setLoginPopupVisible] = useState(false);
- 
 
   useEffect(() => {
     fetchComments();
