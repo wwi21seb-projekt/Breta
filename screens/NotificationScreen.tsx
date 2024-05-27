71d5c0bd7b3af9ddf80fb31055bfb8079db2aa9e
Nico1808 <niconiebisch@gmail.com>
2024-05-27 10:13:54 +0200

    test precommit prettier

--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -92,11 +92,10 @@ export default function App() {
   const responseListener = useRef<Notifications.Subscription>();
 
   useEffect(() => {
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
+  notificationListener.current =
+    Notifications.addNotificationReceivedListener((notification) => {
         setNotification(notification);
       });
-
     responseListener.current =
       Notifications.addNotificationResponseReceivedListener((response) => {
         console.log(response);
