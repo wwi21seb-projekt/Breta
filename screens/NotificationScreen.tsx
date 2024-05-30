88aa2657646b242a8a629478aac1b43cba084687
marceleitel <marcel.eitel@extensivereality.de>
2024-05-30 15:38:26 +0200

    Notification screen with repost and follow notifications

--- screens/NotificationScreen.tsx
+++ screens/NotificationScreen.tsx
@@ -1,8 +1,16 @@
 import { useState, useEffect, useRef } from "react";
-import { Text, View, Button, Platform } from "react-native";
+import { Text, View, Image, Button, Platform, FlatList, TouchableOpacity } from "react-native";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
+import { SHADOWS } from "../theme";
+import { useAuth } from "../authentification/AuthContext";
+import { baseUrl } from "../env";
+import { Notification } from "../components/types/Notification"
+import NotificationTab from "../components/NotificationTab";
+import { useIsFocused } from "@react-navigation/native";
+import ErrorComp from "../components/ErrorComp";
+
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -84,14 +92,22 @@ export async function registerForPushNotificationsAsync() {
 }
 
 export default function App() {
+  const { token, user } = useAuth();
   const [expoPushToken] = useState("");
   const [notification, setNotification] = useState<
     Notifications.Notification | undefined
   >(undefined);
+  const [notificationUser, setNotificationUser] = useState<Notification[]>([])
+  const [errorText, setErrorText] = useState("")
   const notificationListener = useRef<Notifications.Subscription>();
   const responseListener = useRef<Notifications.Subscription>();
+  const isFocused = useIsFocused();
 
   useEffect(() => {
+    if (isFocused) {
+      fetchNotifications();
+    }
+
     notificationListener.current =
       Notifications.addNotificationReceivedListener((notification) => {
         setNotification(notification);
@@ -109,29 +125,68 @@ export default function App() {
       responseListener.current &&
         Notifications.removeNotificationSubscription(responseListener.current);
     };
-  }, []);
+  }, [isFocused]);
 
+  const fetchNotifications = async () => {
+
+    let response;
+
+    try {
+      response = await fetch(`${baseUrl}notifications`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      let data = await response.json()
+      switch (response.status) {
+        case 200:
+          const userNotification = await data.records 
+          setNotificationUser(userNotification)
+          break;
+        case 401:
+          setErrorText(data.error.message)
+          break;
+        default:
+          setErrorText("Something went wrong, please try again later.")
+      }
+    } catch (error) {
+      setErrorText(
+        "There are issues communicating with the server, please try again later.",
+      );
+    }
+    
+  }
+  if (errorText !== "") {
+    return <ErrorComp errorText={errorText} />;
+  }
+  else{
   return (
-    <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
-    >
-      <Text>Your Expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        <Text>
-          Title: {notification && notification.request.content.title}{" "}
-        </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>
-          Data:{" "}
-          {notification && JSON.stringify(notification.request.content.data)}
-        </Text>
-      </View>
-      <Button
-        title="Press to Send Notification"
-        onPress={async () => {
-          await sendPushNotification(expoPushToken);
-        }}
-      />
+    <View className="flex-1 bg-white">
+      {notificationUser.length > 0 ? (
+        <FlatList
+        data={notificationUser}
+        keyExtractor={(item) => item.notificationId}
+        renderItem={({ item }) => (
+          <NotificationTab
+            notificationId={item.notificationId}
+            timestamp={item.timestamp}
+            notificationType={item.notificationType}
+            username={item.user.username}
+            profilePictureUrl={item.user.profilePictureUrl}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        onEndReachedThreshold={0.2}
+        className="bg-white"
+      ></FlatList>
+      ) : (
+        <View className="items-center justify-center flex-0">
+          <Text className="text-xl text-lightgray">You have no new notifications</Text>      
+        </View>
+        )}
     </View>
   );
+}
 }
