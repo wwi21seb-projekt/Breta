34bba3bc033f8ef60c043ae34d988d7d2e330142
justSchwab <justin.schwab@t-online.de>
2024-06-17 16:48:35 +0200

    updated alpha url

--- env.ts
+++ env.ts
@@ -1,9 +1,9 @@
 // Mockserver URL for apk: http://10.0.2.2:3000/api/
-// Aplha Server URL: https://test.c930.net/api/
+// Aplha Server URL: https://alpha.c930.net/api/
 // Beta Server URL: https://server-beta.de/api/
 // Mabu Mockserver URL: https://projekt-mockserver.mabu2807.de/api/
 
-// Alpha Websocket: wss://test.c930.net/api/
+// Alpha Websocket: wss://alpha.c930.net/api/
 // Beta Websocket: wss://server-beta.de/api/
 
 // URL for development
